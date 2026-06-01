34c9eac63921c5dfbd3687144ed7f1caebe24a29
vkhashimoto <git@knj.email>
2026-06-01 11:58:06 -0300

    feat: add installable package to flake

--- flake.nix
+++ flake.nix
@@ -18,13 +18,25 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        packages.todo = pkgs.stdenv.mkDerivation {
+          pname = "todo";
+          version = "0.1.0";
+          src = ./.;
+          nativeBuildInputs = [ pkgs.zig ];
+          buildPhase = ''
+            zig build install --prefix $out -Doptimize=ReleaseSafe
+          '';
+          dontInstall = true;
+        };
+
+        packages.default = self.packages.${system}.todo;
+
         devShells.default = pkgs.mkShell {
           buildInputs = [
             pkgs.nixfmt
             pkgs.zig
             pkgs.zls
           ];
-
         };
       }
     );
